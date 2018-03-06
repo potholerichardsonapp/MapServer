84c5286cfb6271c159d8e770b57a6d705b7ff03c
Daniel <danr14@gmail.com>
2018-03-06 04:43:24 -0600

    Results now returned as JSON object to data script

--- mapserver/maps/views.py
+++ mapserver/maps/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.shortcuts import render
 from .forms import SearchForm
 from .models import DataReport
+from django.core import serializers
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+
 
 # Create your views here.
 def search_form(request):
@@ -11,15 +15,41 @@ def search_form(request):
         form = SearchForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            #Find the data that matches the search params
+
+            #Get the search params from the valid form
             z_axis = form.cleaned_data['threshold']
+
+            #Package the data that matches the search params
             results = DataReport.objects.filter(z_axis__gte=z_axis)
-            # redirect to a new URL:
-            return HttpResponseRedirect('index.html')
+
+            json_data = []
+
+            #Serialize the data to be returned
+            for result in results:
+                json_obj = dict(
+                    lat = str(result.lat),
+                    long = str(result.long),
+                    accel = str(result.z_axis),
+                    generator = result.generator.pk,
+                    date_time = result.date_time,
+
+                )
+                json_data.append(json_obj)
+
+                data = json.dumps(json_data, default=date_handler)
+
+            # Return data
+            return render(request, 'maps/search.html', {'form': form, 'data': data})
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = SearchForm()
         results = ''
 
-    return render(request, 'maps/search.html', {'form': form}, {'results': results})
+    return render(request, 'maps/search.html', {'form': form})
+
+def date_handler(obj):
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        raise TypeError
--- mapserver/mapserver/urls.py
+++ mapserver/mapserver/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', include('maps.urls')),
+    path('',include('maps.urls')),
 ]

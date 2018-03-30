69719d36311335f52f20a74f6befbae8984d7e16
dongkwonshin <dkshin91@gmail.com>
2018-03-30 12:55:07 -0500

    can now search with emtpy fields

--- mapserver/maps/forms.py
+++ mapserver/maps/forms.py
@@ -4,9 +4,10 @@ from django import forms
 class SearchForm(forms.Form):
    # start_date = forms.DateTimeField(label='Start Date/Time')
     #end_date = forms.DateTimeField(label='End Date/Time')
-    threshold = forms.DecimalField(label='z-axis thresholds')
-    userId = forms.DecimalField(label='user Id')
-    eventId = forms.DecimalField(label='event Id')
+    date_time = forms.DateTimeField(label='time', required=False)
+    threshold = forms.DecimalField(label='z-axis thresholds', required=False)
+    user_id = forms.DecimalField(label='user Id', required=False)
+    event_id = forms.DecimalField(label='event Id', required=False)
 
     ## make this field 'readonly'. toggle touch to designate coordinate? .... SIMPLY CREATE A BUTTON TO SEARCH AREA SHOWN ON SCREEN
     # latitude = forms.DecimalField(label='latitude')
--- mapserver/maps/views.py
+++ mapserver/maps/views.py
@@ -17,12 +17,17 @@ def search_form(request):
         if form.is_valid():
 
             #Get the search params from the valid form
+            date_time = form.cleaned_data['date_time']
             z_axis = form.cleaned_data['threshold']
-            user_id = form.cleaned_data['userId']
-            event_id = form.cleaned_data['userId']
+            user_id = form.cleaned_data['user_id']
+            event_id = form.cleaned_data['event_id']
 
             #Package the data that matches the search params
-            results = DataReport.objects.filter(z_axis__get=z_axis)
+            results = DataReport.objects \
+                .filter(generator_gte=user_id) \
+                .filter(z_axis__gte=z_axis) \
+                .filter(date_time__gte=date_time) \
+                .filter(event_id__gte=event_id)
 
 
             json_data = []
@@ -30,12 +35,13 @@ def search_form(request):
             #Serialize the data to be returned
             for result in results:
                 json_obj = dict(
+                    generator=result.generator.pk,
                     lat = str(result.lat),
                     long = str(result.long),
                     accel = str(result.z_axis),
-                    generator = result.generator.pk,
                     date_time = result.date_time,
-
+                    event_type = result.event_type,
+                    event_id = result.event_id
                 )
                 json_data.append(json_obj)
 
